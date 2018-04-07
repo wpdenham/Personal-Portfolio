49f18a7e8e563daf6e72288e210bb74e9e047ef1
wpdenham <wpdenham@gmail.com>
2018-04-07 15:19:26 -0500

    Additional developer.html styling and images added. Simple script to animate scroll icon.

---
+++ developer/js/script.js
@@ -0,0 +1,29 @@
+
+// function onload(){
+// 	const scrollIcon = document.getElementById("scrollIcon");
+// 	scrollIcon.addEventListener('mouseover', wiggle);
+// 	scrollIcon.addEventListener('mouseleave', wiggle);
+// }
+
+
+// function wiggle(){
+// 	scrollIcon.classList.toggle('bounce');
+// 	}
+
+function onload(){
+	const scrollIcon = document.getElementById("scrollIcon").classList.add('bounce');
+	// scrollIcon.addEventListener('mouseover', wiggle);
+	// scrollIcon.addEventListener('mouseleave', wiggle);
+}
+
+
+// function wiggle(){
+// 	scrollIcon.classList.toggle('bounce');
+// 	}
+
+
+
+
+
+
+

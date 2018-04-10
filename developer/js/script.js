b26bda4d3036cdbd44e4c3ac1f4e3e2a9a77f807
wpdenham <wpdenham@gmail.com>
2018-04-09 22:05:42 -0500

    Added animation for contact forms. Also added on scroll animation for about page education and interest sections.

--- developer/js/script.js
+++ developer/js/script.js
@@ -1,26 +1,38 @@
+function slideUp(){
 
-// function onload(){
-// 	const scrollIcon = document.getElementById("scrollIcon");
-// 	scrollIcon.addEventListener('mouseover', wiggle);
-// 	scrollIcon.addEventListener('mouseleave', wiggle);
-// }
+	const element = document.querySelectorAll('.anim');
+	
+	document.addEventListener('scroll', checkPosition)
+	
+	function checkPosition(){
+	
+	for (i = 0; i < element.length; i++){
+			const elementHeight = element[i].clientHeight;
+			let winHeight = window.innerHeight;
+			let yPos = window.scrollY;
+			let scrollPos = yPos + winHeight;
+			let elPos = element[i].getBoundingClientRect().top + yPos + elementHeight;
 
+			if (scrollPos > elPos - 750){
+				element[i].classList.add('fadeInUp');
+			}	
 
-// function wiggle(){
-// 	scrollIcon.classList.toggle('bounce');
-// 	}
+		}
 
-function onload(){
-	const scrollIcon = document.getElementById("scrollIcon").classList.add('bounce');
-	// scrollIcon.addEventListener('mouseover', wiggle);
-	// scrollIcon.addEventListener('mouseleave', wiggle);
+	}
+
+	
 }
 
-
-// function wiggle(){
-// 	scrollIcon.classList.toggle('bounce');
-// 	}
-
+function expandForm(){
+	const form = document.querySelector('.form');
+	
+	const letsTalkBtn = document.getElementById('letsTalk');
+	
+	letsTalkBtn.addEventListener('click', function(){
+		form.classList.toggle('formhidden')});
+	
+}
 
 
 
